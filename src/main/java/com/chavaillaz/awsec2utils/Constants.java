8d0cde4b5324cdd2b2e0054b21d66f35670868cc
Chavjoh <j.chavaillaz@gmail.com>
2015-05-24 15:47:19 +0200

    Update assembling parameters.

--- src/main/java/com/chavaillaz/awsec2utils/Constants.java
+++ src/main/java/com/chavaillaz/awsec2utils/Constants.java
@@ -7,13 +7,17 @@ package com.chavaillaz.awsec2utils;
  */
 public class Constants {
 	
+	public static final String PROPERTIES_EXTENSION = ".properties";
+	
 	public static final String KEY_PAIR_PREFIX = "AWS-EC2-Utils-";
 	public static final String KEY_PAIR_EXTENSION = ".pem";
+	
 	public static final String PREFIX = "vm ";
+	
 	public static final String AWS_ENDPOINT = "ec2.us-east-1.amazonaws.com";
 	public static final String AWS_SECURITY_GROUP = "AWS-EC2-Utils";
 	public static final String AWS_SECURITY_GROUP_DESCRIPTION = "Default group used by AWS-EC2-Utils.";
-	public static final String PROPERTIES_EXTENSION = ".properties";
+	
 	public static final String VM_FOLDER = "vm/";
 	public static final String VM_EXTENSION = PROPERTIES_EXTENSION;
 	
